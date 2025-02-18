f37b62589b970388082a86295982fd57ff7fd148
PoojaB528 <104970448+PoojaB528@users.noreply.github.com>
2025-02-18 16:55:02 +0530

    Update TestGreeter.java for fail build

--- Jenkins_Upgradev3-master/maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ Jenkins_Upgradev3-master/maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -26,6 +26,13 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
+
+  @Test
+  public void intentinallyFailed() {
+    String someone = "World";
+
+    assertTrue(true);
+  }
   
 
   @Test
